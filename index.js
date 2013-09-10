a7521750ae3d98a5428eb6c1da788743d04640b1
Chris Morrell <github@rchy.net>
2013-09-10 10:34:05 -0400

    Expose a little more of the library

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // Export
+exports.Response = require('./lib/Response.js');
+
 exports.OkResponse = require('./lib/OkResponse.js');
 exports.ErrorResponse = require('./lib/ErrorResponse.js');
+
+exports.RESPONSE_CODES = Response.codes;
