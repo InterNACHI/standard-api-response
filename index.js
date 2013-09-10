af849069c72cf3369ba6d00962b4a92e1a8ca33d
Chris Morrell <github@rchy.net>
2013-09-10 10:34:24 -0400

    Renamed codes property

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ exports.Response = require('./lib/Response.js');
 exports.OkResponse = require('./lib/OkResponse.js');
 exports.ErrorResponse = require('./lib/ErrorResponse.js');
 
-exports.RESPONSE_CODES = Response.codes;
+exports.codes = Response.codes;
